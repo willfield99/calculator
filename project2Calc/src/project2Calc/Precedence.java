539427279333fab7da355c22429ef81fa563ae84
willfield <52052049+willfield99@users.noreply.github.com>
2019-11-18 15:31:51 -0500

    m

--- project2Calc/src/project2Calc/Precedence.java
+++ project2Calc/src/project2Calc/Precedence.java
@@ -7,10 +7,16 @@ import java.util.Comparator;
  * 
  * Precedence is a comparator class that is used to compare operators precedence in PEMDAS order. '%' is the same precedence As muliplication and division.
  * This class is used by the toPostFix method of StackCalculator
- * 
  */
-public class Precedence implements Comparator<Character>{
 
+public class Precedence implements Comparator<Character>{
+	
+	/*Pre Condition-c1 and c2 are characters being compared in the toPostFix method to be pushed to the operator stack
+	 * 
+	 * Post Condition- returns a positive integer if c1 has greater precedence in order of operations, negative integer if
+	 * c2 has greater precedence, and 0 if the precedence of the 2 operators is equal.
+	 */
+	
 	@Override
 	public int compare(Character c1, Character c2) {//compares 2 characters precedence
 		int i1, i2;
@@ -19,6 +25,11 @@ public class Precedence implements Comparator<Character>{
 		
 		return i1 - i2;
 	}
+	
+	/*Pre Condition- char c is an operator that has passed isOperator in StackCalculator
+	 * 
+	 * Post Condition- an int used to represent the operators precedence is returned to the compare function
+	 */
 	
 	private int intassign(char c) {//assigns int values to characters precedence to compare them
 		int val = 0;
